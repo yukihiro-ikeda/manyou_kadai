74e055386340de70cbf967199035e57a33203040
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-29 21:47:55 +0900

    Model Specとテストデータ用のFactoryBotを作成

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(title: '', content: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        # ここに内容を記載する
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        # ここに内容を記載する
+      end
+    end
+  end
+  #pending "add some examples to (or delete) #{__FILE__}"
+end
