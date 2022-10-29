3e05d48716f6cbb073da522a949aa8ab65e07354
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-29 17:13:17 +0900

    タスク登録にバリデーションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,29 +4,44 @@ class TasksController < ApplicationController
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
-  
+    @task = Task.new(params_valid)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
   def edit
-
+    @task = Task.find(params[:id])
   end
 
   def update
-
+    @task = Task.find(params[:id]) 
+    if @task.update(params_valid)  
+      redirect_to tasks_path, notice: "タスクを更新しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
-
+  private
+  def params_valid
+    params.require(:task).permit(:name, :detail)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100}
+  validates :detail, presence: true, length: { maximum: 500 }
 end
