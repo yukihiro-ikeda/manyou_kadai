08516bc69f87e0657f747fdf91c770c27f5ebd7e
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-30 21:43:12 +0900

    systemspecとmodelspecを記述

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task do
-    
+    name { 'name' }
+    detail { 'detail' }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,17 +4,21 @@ RSpec.describe Task, type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+        task = Task.new(name: '', detail: '失敗テスト')
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
+        task = Task.new(name: '失敗テスト', detail: '')
+        expect(task).not_to be_valid
         # ここに内容を記載する
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
+        task = Task.new(name: '成功テスト', detail: '成功テスト')
+        expect(task).to be_valid
         # ここに内容を記載する
       end
     end
