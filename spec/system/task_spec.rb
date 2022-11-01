786dd9897740cc24513ff1c83c58a5599ae84a06
yukihiro ikeda <headlessrecord@gmail.com>
2022-11-01 16:59:59 +0900

    タスク一覧を作成日の降順で表示+systemspecに降順表示のテストを追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -17,9 +17,19 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
+        FactoryBot.create(:task, name: 'task')
         visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        FactoryBot.create(:task, name: '11月1日にすること')
+        FactoryBot.create(:task, name: '11月2日にすること')
+        visit tasks_path
+        list = all('.task_list')
+        expect(list[1]).to have_content "11月1日にすること"
+        expect(list[0]).to have_content "11月2日にすること"
       end
     end
   end
