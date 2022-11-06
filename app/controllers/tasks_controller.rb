c2c3df53e5387f5a699d38ca2e42de5a5d678d3f
yukihiro ikeda <headlessrecord@gmail.com>
2022-11-06 22:46:33 +0900

    タスク名の曖昧検索機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,15 @@
 class TasksController < ApplicationController
   def index
     if params[:sort_expired]
-      @tasks = Task.all.order(expired_at: "DESC")    
+      @tasks = Task.all.order(expired_at: "DESC")
     else
       @tasks = Task.all
     end
+    #params[:search]を#params[:task]に変えたら機能した！
+    if params[:task].present?
+      @tasks = Task.where('name LIKE ?', "%#{params[:task][:name]}%")
+    end
+    # @tasks = @tasks.where('title LIKE ?', "%#{params[:search]}%") if params[:search].present?
   end
 
   def show
@@ -44,8 +49,12 @@ class TasksController < ApplicationController
     redirect_to tasks_path, notice: "タスクを削除しました"
   end
 
+  # def search
+  #   @tasks = current_user.tasks.looks(params[:status], params[:name_cont]).page(params[:page])
+  # end
+
   private
   def params_valid
-    params.require(:task).permit(:name, :detail, :expired_at)
+    params.require(:task).permit(:name, :detail, :expired_at, :status　)
   end
 end
