4efb10af4f56167954bd03c5332a04eda18f0126
yukihiro ikeda <headlessrecord@gmail.com>
2022-11-09 00:28:26 +0900

    一覧画面にページネーションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,37 @@
 class TasksController < ApplicationController
   def index
+    
     if params[:sort_expired]
       @tasks = Task.all.order(expired_at: "DESC")
+      @tasks = @tasks.all.page(params[:page]).per(5)
     else
       @tasks = Task.all
+      @tasks = @tasks.all.page(params[:page]).per(5)
     end
 
     if params[:sort_priority]
       @tasks = Task.all.order(priority: :asc)
+      @tasks = @tasks.all.page(params[:page]).per(5)
     end
-    
+
     if params[:task].present? 
       if params[:task][:name].present? && params[:task][:status].present?
       @tasks = Task.where(status: params[:task][:status]).where('name LIKE ?', "%#{params[:task][:name]}%")
+      @tasks = @tasks.all.page(params[:page]).per(5)
     #params[:search]を#params[:task]に変更して機能した
       elsif params[:task][:name].present?
         @tasks = Task.where('name LIKE ?', "%#{params[:task][:name]}%")
+        @tasks = @tasks.all.page(params[:page]).per(5)
       elsif params[:task][:status].present?
         @tasks = Task.where(status: params[:task][:status])
+        @tasks = @tasks.all.page(params[:page]).per(5)
         # binding.irb
       end
     end
     
-    
     if params[:status].present?
       @tasks = Task.where(status: params[:status])
+      @tasks = @tasks.all.page(params[:page]).per(5)
       # binding.irb
     end
     # @tasks = @tasks.where('title LIKE ?', "%#{params[:search]}%") if params[:search].present?
