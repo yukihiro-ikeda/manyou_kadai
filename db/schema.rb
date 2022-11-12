8c7018f57522cc49498320dcb1f5815cebc0c609
yukihiro ikeda <headlessrecord@gmail.com>
2022-11-13 01:48:42 +0900

    CreateUsersマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_042104) do
+ActiveRecord::Schema.define(version: 2022_11_12_164556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_11_09_042104) do
     t.integer "status"
     t.integer "priority"
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
